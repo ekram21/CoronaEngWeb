f57fdacbfbc6da96950e0bdc1d978232fdef37fe
Sk. Sajidul Kadir <sheikh.sajid522@gmail.com>
2018-11-01 14:09:34 +0600

    Added dropdown menus to Nav

--- js/nav.js
+++ js/nav.js
@@ -1,21 +1,29 @@
 window.addEventListener('scroll', function() {
-    function changeNavLinkColor(color) {
+    function changeNavFontColor(color) {
         let links = document.getElementById('main-navigation').getElementsByTagName('a');
+        let dropDownButtons = document.getElementById('main-navigation').getElementsByTagName('button');
 
+        // change font color of the a tags in the nav 
         for(let i = 0; i < links.length; i++) {
             let link = links[i];
             link.setAttribute('style', 'color: ' + color);
         }
+        
+        // change font color of the dropdown buttons in the nav 
+        for(let i = 0; i < dropDownButtons.length; i++) {
+            let button = dropDownButtons[i];
+            button.setAttribute('style', 'color: ' + color);
+        }
     }
     
     let scrolledPx = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    var nav = document.getElementsByTagName('nav')[0];
+    let nav = document.getElementsByTagName('nav')[0];
     
     if (scrolledPx > 200) {
         nav.className = 'solid-bg';
-        changeNavLinkColor('grey');
+        changeNavFontColor('grey');
     } else {
         nav.className = '';
-        changeNavLinkColor('white');
+        changeNavFontColor('white');
     }
 });
