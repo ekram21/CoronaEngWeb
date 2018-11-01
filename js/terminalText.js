eccd68898f5cc8e4892613eb6d0000ffb20a71bf
Ekram21 <ekram_lol@hotmail.com>
2018-11-02 02:37:41 +0600

    changed the terminal text speed a bit

--- js/terminalText.js
+++ js/terminalText.js
@@ -27,14 +27,14 @@ function consoleText(words, id, colors) {
         target.setAttribute('style', 'color:' + colors[0])
         letterCount += x;
         waiting = false;
-      }, 200)
+      }, 1000)
     } else if (letterCount === words[0].length + 1 && waiting === false) {
       waiting = true;
       window.setTimeout(function() {
         x = -1;
         letterCount += x;
         waiting = false;
-      }, 200)
+      }, 1000)
     } else if (waiting === false) {
       target.innerHTML = words[0].substring(0, letterCount)
       letterCount += x;
