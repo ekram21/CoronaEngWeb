70b072cc8eec0f6fd636ff9e2f5c57ba453afde3
Ekram21 <ekram_lol@hotmail.com>
2018-11-02 21:09:07 +0600

    fixed some stuff of the console text

--- js/terminalText.js
+++ js/terminalText.js
@@ -2,7 +2,7 @@
 
 
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['Innovative and Reliable Engineering with Zero Compromises', 'Console Text', 'Example Text'], 'text',['white','white','white']);
+consoleText(['Engineering, Procurement & Construction (EPC)'], 'text',['white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
@@ -13,11 +13,14 @@ function consoleText(words, id, colors) {
   var waiting = false;
   var target = document.getElementById(id)
   target.setAttribute('style', 'color:' + colors[0])
-  window.setInterval(function() {
 
+
+  window.setInterval(function() {
     if (letterCount === 0 && waiting === false) {
       waiting = true;
       target.innerHTML = words[0].substring(0, letterCount)
+
+      console.log('Letter count was 0')
       window.setTimeout(function() {
         var usedColor = colors.shift();
         colors.push(usedColor);
@@ -28,27 +31,37 @@ function consoleText(words, id, colors) {
         letterCount += x;
         waiting = false;
       }, 1000)
+
     } else if (letterCount === words[0].length + 1 && waiting === false) {
-      waiting = true;
-      window.setTimeout(function() {
-        x = -1;
-        letterCount += x;
-        waiting = false;
-      }, 1000)
+
+      //this sets the incremenet variable x to -1 so that letters are now erased sequentially
+      console.log('second else if')
+      // waiting = true;
+      // window.setTimeout(function() {
+      //   x = -1;
+      //   letterCount += x;
+      //   waiting = false;
+      // }, 1000)
     } else if (waiting === false) {
+
+      //if x is positive a letter is added sequentially to the string in display
+      //if x is negative then a letter is subtracted
       target.innerHTML = words[0].substring(0, letterCount)
       letterCount += x;
     }
-  }, 120)
-  window.setInterval(function() {
-    if (visible === true) {
-      con.className = 'console-underscore hidden'
-      visible = false;
+  }, 40)
 
-    } else {
-      con.className = 'console-underscore'
 
-      visible = true;
-    }
-  }, 400)
+  // window.setInterval(function() {
+  //   if (visible === true) {
+  //     con.className = 'console-underscore hidden'
+  //     visible = false;
+
+  //   } else {
+  //     con.className = 'console-underscore'
+
+  //     visible = true;
+  //   }
+  // }, 400)
+
 }
