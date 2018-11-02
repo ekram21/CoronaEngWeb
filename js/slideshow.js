456802fcb24c21160064e54766ca57a278c659e1
Ekram <ekram_lol@hotmail.com>
2018-11-03 04:44:06 +0600

    autoplay changes and text changes

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,6 +3,8 @@
 $(document).ready(function () {
     $('.Slideshow').slick({
         dots: true,
-        autoplay: true
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 500,
     });
 });
