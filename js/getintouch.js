c51cb918fec3b89f64ab6001db1d3782c3795a51
TowhidAust <towhidaustcse33@gmail.com>
2018-11-05 16:48:35 +0600

    contact form has been updated

---
+++ js/getintouch.js
@@ -0,0 +1,37 @@
+function borderBottom() {
+    var b = document.getElementsByClassName("underline1");
+    b[0].style.borderBottom = "1px solid #E91E63";
+};
+document.getElementsByClassName("underline1")[0].onclick = function () {
+    borderBottom();
+};
+
+
+function borderBottom2() {
+    var c = document.getElementsByClassName("underline2");
+    c[0].style.borderBottom = "1px solid #E91E63";
+};
+
+document.getElementsByClassName("underline2")[0].onclick = function () {
+    borderBottom2();
+};
+
+
+
+function borderBottom3() {
+    var c = document.getElementsByClassName("subject-input");
+    c[0].style.borderBottom = "1px solid #E91E63";
+};
+
+document.getElementsByClassName("subject-input")[0].onclick = function () {
+    borderBottom3();
+};
+
+function borderBottom4() {
+    var c = document.getElementsByClassName("text-area");
+    c[0].style.borderBottom = "1px solid #E91E63";
+};
+
+document.getElementsByClassName("text-area")[0].onclick = function () {
+    borderBottom4();
+};
