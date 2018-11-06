1343ea20afd7c927f1efc4adff19e30eee5d0e00
Sk. Sajidul Kadir <sheikh.sajid522@gmail.com>
2018-11-06 17:18:09 +0600

    Added quotation form

--- js/general.js
+++ js/general.js
@@ -1,6 +1,46 @@
 $(document).ready(function() {
     makeSectionExpandable('#clientsContainerExpandable', '#show-more-clients', '#hide-clients', '#clientsContainer', '.client');
     makeSectionExpandable('#productCardExpandable', '#show-more-products', '#hide-products', '#productCont', '.productOuterCard');
+
+    $('#chooseFile').bind('change', function () {
+        var filename = $("#chooseFile").val();
+        if (/^\s*$/.test(filename)) {
+          $(".file-upload").removeClass('active');
+          $("#noFile").text("No file chosen..."); 
+        }
+        else {
+          $(".file-upload").addClass('active');
+          $("#noFile").text(filename.replace("C:\\fakepath\\", "")); 
+        }
+    });
+
+    $('.quotation').click(function() {
+        $('#get-quotation-form').fadeIn('fast');
+        var deviceType = $(this).siblings('.itemHeading').text();
+        var files = []
+        
+        let fileButton = document.getElementById('chooseFile');
+        
+        fileButton.addEventListener('change', function(e) {
+            files = e.target.files;
+
+            console.log(files);
+        });
+
+        $('#close-quote-form-button').click(function(e) {
+            e.preventDefault();
+            console.log('closing!')
+            $(this).off('click');
+            $('#get-quotation-form').fadeOut('fast');
+        });
+
+        console.log('devType: ' + deviceType);
+        $('#get-quotation-form form').submit(function(e) {
+            e.preventDefault();
+            console.log('submitting');
+        });
+    });
+
 });
 
 function makeSectionExpandable(targetId, showButtonId, hideButtonId, mainSectionId, innerElement) {
