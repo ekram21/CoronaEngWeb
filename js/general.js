c91ada147fbd7898b83e2bea6e67c1885fe3b69d
Sk. Sajidul Kadir <sheikh.sajid522@gmail.com>
2018-11-07 10:15:05 +0600

    File upload feature now working

--- js/general.js
+++ js/general.js
@@ -1,46 +1,75 @@
 $(document).ready(function() {
     makeSectionExpandable('#clientsContainerExpandable', '#show-more-clients', '#hide-clients', '#clientsContainer', '.client');
     makeSectionExpandable('#productCardExpandable', '#show-more-products', '#hide-products', '#productCont', '.productOuterCard');
+    var files = []
+    var deviceType = '';
 
-    $('#chooseFile').bind('change', function () {
+    $('#chooseFile').change(e => {
         var filename = $("#chooseFile").val();
+        files = e.target.files;
+        
         if (/^\s*$/.test(filename)) {
-          $(".file-upload").removeClass('active');
-          $("#noFile").text("No file chosen..."); 
+            $(".file-upload").removeClass('active');
+            $("#noFile").text("No file chosen..."); 
         }
         else {
-          $(".file-upload").addClass('active');
-          $("#noFile").text(filename.replace("C:\\fakepath\\", "")); 
+            $(".file-upload").addClass('active');
+            $("#noFile").text(files.length + ' file(s) selected'); 
         }
     });
 
+    $('#close-quote-form-button').click(e => {
+        e.preventDefault();
+        $('#get-quotation-form').fadeOut('fast');
+    });
+    
     $('.quotation').click(function() {
         $('#get-quotation-form').fadeIn('fast');
-        var deviceType = $(this).siblings('.itemHeading').text();
-        var files = []
-        
-        let fileButton = document.getElementById('chooseFile');
-        
-        fileButton.addEventListener('change', function(e) {
-            files = e.target.files;
-
-            console.log(files);
-        });
-
-        $('#close-quote-form-button').click(function(e) {
-            e.preventDefault();
-            console.log('closing!')
-            $(this).off('click');
-            $('#get-quotation-form').fadeOut('fast');
-        });
-
-        console.log('devType: ' + deviceType);
-        $('#get-quotation-form form').submit(function(e) {
-            e.preventDefault();
-            console.log('submitting');
-        });
+        deviceType = $(this).siblings('.itemHeading').text();
     });
+    
+    $('#get-quotation-form form').submit(function(e) {
+        e.preventDefault();
+        var formData = $(this).serializeArray();
+        var metaData = {};
+        let filesUploaded = 0;
+        let fileFormatRegex = /(\.pdf|\.doc|\.docx)$/i;
 
+        for (var entry of formData) metaData[entry.name] = entry.value;
+        
+        if (!files.length){
+            $('#upload-state').text('You must select atleast 1 file for upload...');
+            $('#upload-state').fadeIn('fast');
+        } 
+        else {
+            for (var file of files) {
+                $('#upload-state').text('Uploading files...');
+                $('#upload-state').show();
+                
+                var storageRef = firebase.storage().ref(deviceType + '/' + metaData.email + '_' + file.name);
+                
+                if (fileFormatRegex.test(file.name)) {
+                    storageRef.put(file, metaData).on(
+                        'state_changed',
+                        snap => {}, 
+                        error =>  $('#upload-state').text(`${error}`),
+                        () => {
+                            filesUploaded++;
+                            $('#upload-state').text(`${filesUploaded} out of ${files.length} files uploaded`);
+        
+                            if (filesUploaded === files.length) {
+                                $('#upload-state').text('Your files have been uploaded, we will get back to you soon!');
+                                $('#get-quotation-form form').hide();
+                                // $('#get-quotation-form').fadeOut('fast');
+                            }
+                        }
+                    );
+                } else
+                    $('#upload-state').text('Only PDF and MS Word (doc/docx) files allowed');
+    
+            }
+        }
+    });
 });
 
 function makeSectionExpandable(targetId, showButtonId, hideButtonId, mainSectionId, innerElement) {
